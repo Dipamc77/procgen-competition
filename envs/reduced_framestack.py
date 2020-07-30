c415fef6b172df47be3c6354effc43c8a64179c4
r2d2 <dipamc77@gmail.com>
2020-07-31 03:46:09 +0530

    reduced frame stack

---
+++ envs/reduced_framestack.py
@@ -0,0 +1,55 @@
+import numpy as np
+from gym.spaces import Box
+from gym import Wrapper
+from skimage.measure import block_reduce
+
+from ray.tune import registry
+
+from envs.procgen_env_wrapper import ProcgenEnvWrapper
+
+class ReducedFrameStack(Wrapper):
+    def __init__(self, env, num_stack):
+        super().__init__(env)
+        self.num_stack = num_stack
+        wos = env.observation_space  # wrapped ob space
+        assert len(wos.shape) == 3, "Only supports images"
+        assert np.prod(wos.shape[:2]) % 16 == 0, "Only supports for 4x4 reduction"
+        
+        self.flatlen = np.prod(wos.shape)
+        self.oldlen = np.prod(wos.shape[:2])//16
+        oblen = self.flatlen + self.oldlen * (num_stack - 1)
+        
+        lval, hval = np.min(wos.low), np.max(wos.high)
+        low = np.ones((1,1,oblen), wos.dtype)*lval
+        high = np.ones((1,1,oblen), wos.dtype)*hval
+        self.observation_space = Box(low=low, high=high, dtype=wos.dtype)
+
+        self.stackedobs = np.zeros(low.shape, wos.dtype)
+        self.prev_obs = np.zeros(wos.shape, wos.dtype)
+        
+        self.newobssh = (1, 1, self.flatlen)
+        self.oldobssh = (1, 1, self.oldlen)
+    
+
+    def step(self, action):
+        observation, reward, done, info = self.env.step(action)
+        self.stackedobs[...,self.flatlen + self.oldlen:] = self.stackedobs[...,self.flatlen:-self.oldlen]
+        gray = np.mean(self.prev_obs, axis=-1)
+        gray_res = block_reduce(gray, (4, 4), np.mean)
+        self.stackedobs[...,self.flatlen:self.flatlen + self.oldlen] = np.reshape(gray_res, self.oldobssh)
+        self.stackedobs[...,:self.flatlen] = np.reshape(observation, self.newobssh)
+        self.prev_obs = observation
+        return self.stackedobs, reward, done, info
+
+    def reset(self, **kwargs):
+        observation = self.env.reset(**kwargs)
+        self.stackedobs[...] = 0
+        self.stackedobs[...,:self.flatlen] = np.reshape(observation, self.newobssh)
+        self.prev_obs = observation
+        return self.stackedobs
+    
+# Register Env in Ray
+registry.register_env(
+    "reduced_frame_stack",
+    lambda config: ReducedFrameStack(ProcgenEnvWrapper(config), 4)
+)
--- models/impala_framestack.py
+++ models/impala_framestack.py
@@ -4,6 +4,8 @@ from ray.rllib.models import ModelCatalog
 
 tf = try_import_tf()
 
+import numpy as np
+
 def conv_layer(depth, name):
     return tf.keras.layers.Conv2D(
         filters=depth, kernel_size=3, strides=1, padding="same", name=name
@@ -51,6 +53,54 @@ class ImpalaFSCNN(TFModelV2):
         
         x = conv_sequence(x, depths[0], extra_inputs=None, prefix="seq0")
         x = conv_sequence(x, depths[1], extra_inputs=pfs_ds, prefix="seq1")
+        x = conv_sequence(x, depths[2], extra_inputs=None, prefix="seq2")
+
+        x = tf.keras.layers.Flatten()(x)
+        x = tf.keras.layers.ReLU()(x)
+        x = tf.keras.layers.Dense(units=256, name="project")(x)
+        x = tf.keras.layers.LayerNormalization(name='lnorm')(x)
+        x = tf.keras.layers.Activation('tanh', name='hidden')(x)
+        logits = tf.keras.layers.Dense(units=num_outputs, name="pi")(x)
+        value = tf.keras.layers.Dense(units=1, name="vf")(x)
+        self.base_model = tf.keras.Model(inputs, [logits, value])
+        self.register_variables(self.base_model.variables)
+
+    def forward(self, input_dict, state, seq_lens):
+        # explicit cast to float32 needed in eager
+        obs = tf.cast(input_dict["obs"], tf.float32)
+        logits, self._value = self.base_model(obs)
+        return logits, state
+
+    def value_function(self):
+        return tf.reshape(self._value, [-1])
+    
+
+class ImpalaReducedFS(TFModelV2):
+    """
+    Network from IMPALA paper implemented in ModelV2 API.
+
+    Based on https://github.com/ray-project/ray/blob/master/rllib/models/tf/visionnet_v2.py
+    and https://github.com/openai/baselines/blob/9ee399f5b20cd70ac0a871927a6cf043b478193f/baselines/common/models.py#L28
+    """
+
+    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+        super().__init__(obs_space, action_space, num_outputs, model_config, name)
+
+        depths = [32, 64, 64]
+        fstack = 4
+        imsize = [64,64,3]
+        reducedsize = [16,16]
+        flatlen = np.prod(imsize)
+        
+        inputs = tf.keras.layers.Input(shape=obs_space.shape, name="observations")
+        scaled_inputs = tf.cast(inputs, tf.float32) / 255.0
+        
+        x, prev = scaled_inputs[...,:flatlen], scaled_inputs[...,flatlen:]
+        x = tf.reshape(x, (-1,*imsize))
+        prev = tf.reshape(prev, (-1,*reducedsize,fstack-1))
+        
+        x = conv_sequence(x, depths[0], extra_inputs=None, prefix="seq0")
+        x = conv_sequence(x, depths[1], extra_inputs=prev, prefix="seq1")
         x = conv_sequence(x, depths[2], extra_inputs=None, prefix="seq2")
 
         x = tf.keras.layers.Flatten()(x)
@@ -122,6 +172,8 @@ class ImpalaFrameStack(TFModelV2):
 # Register model in ModelCatalog
 ModelCatalog.register_custom_model("impala_framestack", ImpalaFSCNN)
 ModelCatalog.register_custom_model("impala_fs_gym", ImpalaFrameStack)
+ModelCatalog.register_custom_model("impala_reduced_fs", ImpalaReducedFS)
+
 
 
 
