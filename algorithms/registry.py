08f6fbc7e17b71e8cbc4df553d83a4cc0a760ed8
r2d2 <dipamc77@gmail.com>
2020-07-31 11:52:50 +0530

    custom ppo register

--- algorithms/registry.py
+++ algorithms/registry.py
@@ -21,8 +21,13 @@ def _import_random_policy():
     from .random_policy.trainer import RandomPolicyTrainer
     return RandomPolicyTrainer
 
+def _import_custom_ppo_agent():
+    from .custom_ppo.ppo import PPOTrainer
+    return PPOTrainer
+
 
 CUSTOM_ALGORITHMS = {
     "custom/CustomRandomAgent": _import_custom_random_agent,
-    "RandomPolicy": _import_random_policy
+    "RandomPolicy": _import_random_policy,
+    "CustomPPOAgent": _import_custom_ppo_agent
 }
