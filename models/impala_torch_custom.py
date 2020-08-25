37bff0040c68539564b8b6880ca435326afa5eb8
r2d2 <dipamc77@gmail.com>
2020-08-25 12:53:18 +0530

    glorot init

--- models/impala_torch_custom.py
+++ models/impala_torch_custom.py
@@ -8,10 +8,16 @@ torch, nn = try_import_torch()
 
 
 class ResidualBlock(nn.Module):
-    def __init__(self, channels):
+    def __init__(self, channels, init_glorot=False):
         super().__init__()
         self.conv0 = nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=3, padding=1)
         self.conv1 = nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=3, padding=1)
+        if init_glorot:
+            nn.init.xavier_uniform_(self.conv0.weight)
+            nn.init.zeros_(self.conv0.bias)
+            nn.init.xavier_uniform_(self.conv1.weight)
+            nn.init.zeros_(self.conv1.bias)
+
     
     def forward(self, x):
         inputs = x
@@ -23,13 +29,16 @@ class ResidualBlock(nn.Module):
 
 
 class ConvSequence(nn.Module):
-    def __init__(self, input_shape, out_channels):
+    def __init__(self, input_shape, out_channels, init_glorot=False):
         super().__init__()
         self._input_shape = input_shape
         self._out_channels = out_channels
         self.conv = nn.Conv2d(in_channels=self._input_shape[0], out_channels=self._out_channels, kernel_size=3, padding=1)
-        self.res_block0 = ResidualBlock(self._out_channels)
-        self.res_block1 = ResidualBlock(self._out_channels)
+        self.res_block0 = ResidualBlock(self._out_channels, init_glorot)
+        self.res_block1 = ResidualBlock(self._out_channels, init_glorot)
+        if init_glorot:
+            nn.init.xavier_uniform_(self.conv.weight)
+            nn.init.zeros_(self.conv.bias)
 
     def forward(self, x, pool=True):
         x = self.conv(x)
@@ -62,22 +71,28 @@ class ImpalaCNN(TorchModelV2, nn.Module):
         self.device = device
         depths = model_config['custom_options'].get('depths') or [16, 32, 32]
         nlatents = model_config['custom_options'].get('nlatents') or 256
+        init_glorot = model_config['custom_options'].get('init_glorot') or False
         
         h, w, c = obs_space.shape
         shape = (c, h, w)
 
         conv_seqs = []
         for out_channels in depths:
-            conv_seq = ConvSequence(shape, out_channels)
+            conv_seq = ConvSequence(shape, out_channels, init_glorot)
             shape = conv_seq.get_output_shape()
             conv_seqs.append(conv_seq)
         self.conv_seqs = nn.ModuleList(conv_seqs)
         self.hidden_fc = nn.Linear(in_features=shape[0] * shape[1] * shape[2], out_features=nlatents)
+        if init_glorot:
+            nn.init.xavier_uniform_(self.hidden_fc.weight)
+            nn.init.zeros_(self.hidden_fc.bias)
         self.pi_fc = nn.Linear(in_features=nlatents, out_features=num_outputs)
         nn.init.orthogonal_(self.pi_fc.weight, gain=0.01)
+        nn.init.zeros_(self.pi_fc.bias)
         self.value_fc = nn.Linear(in_features=nlatents, out_features=1)
         nn.init.orthogonal_(self.value_fc.weight, gain=1)
-        self.layernorm = nn.LayerNorm(nlatents)
+        nn.init.zeros_(self.value_fc.bias)
+#         self.layernorm = nn.LayerNorm(nlatents)
 
     
     @override(TorchModelV2)
@@ -91,9 +106,9 @@ class ImpalaCNN(TorchModelV2, nn.Module):
         x = torch.flatten(x, start_dim=1)
         x = nn.functional.relu(x)
         x = self.hidden_fc(x)
-        x = self.layernorm(x)
-        x = torch.tanh(x)
-#         x = nn.functional.relu(x)
+#         x = self.layernorm(x)
+#         x = torch.tanh(x)
+        x = nn.functional.relu(x)
         logits = self.pi_fc(x)
         value = self.value_fc(x)
         self._value = value.squeeze(1)
