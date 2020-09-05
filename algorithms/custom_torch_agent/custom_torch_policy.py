5ccbb8ff77cc37865da2768d6b56f6c5b3410d40
r2d2 <dipamc77@gmail.com>
2020-09-05 22:17:41 +0530

    advantage normalization bugfixes

--- algorithms/custom_torch_agent/custom_torch_policy.py
+++ algorithms/custom_torch_agent/custom_torch_policy.py
@@ -167,10 +167,12 @@ class CustomTorchPolicy(TorchPolicy):
         for _ in range(noptepochs):
             np.random.shuffle(inds)
             normalized_advs = returns - values
+            # Can do this because actual_batch_size is a multiple of mem_limited_batch_size
             for start in range(0, nbatch, self.actual_batch_size):
                 end = start + self.actual_batch_size
-                advs_slice = normalized_advs[start:end]
-                normalized_advs[start:end] = np.mean(advs_slice) / (np.std(advs_slice) + 1e-8)
+                mbinds = inds[start:end]
+                advs_batch = normalized_advs[mbinds].copy()
+                normalized_advs[mbinds] = (advs_batch - np.mean(advs_batch)) / (np.std(advs_batch) + 1e-8) 
             for start in range(0, nbatch, nbatch_train):
                 end = start + nbatch_train
                 mbinds = inds[start:end]
