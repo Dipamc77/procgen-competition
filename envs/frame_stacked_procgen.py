ee026cb7ac213eb436e8589870914a2761d8abd8
r2d2 <dipamc77@gmail.com>
2020-10-18 17:01:26 +0530

    faster frame stack 2

--- envs/frame_stacked_procgen.py
+++ envs/frame_stacked_procgen.py
@@ -30,11 +30,36 @@ class FrameStackByChannels(Wrapper):
         observation = self.env.reset(**kwargs)
         self.stackedobs = np.tile(observation, self.num_stack)
         return self.stackedobs
+
+class FasterFrameStack2(Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        
+        low = np.tile(env.observation_space.low, 2)
+        high = np.tile(env.observation_space.high, 2)
+        
+        self.frames = low.copy()
+        self.old_obs = env.observation_space.low.copy()
+        self.observation_space = Box(low=low, high=high, dtype=self.observation_space.dtype)
+
+    def step(self, action):
+        observation, reward, done, info = self.env.step(action)
+        self.stackedobs[...,:-observation.shape[-1]] = self.old_obs.copy()
+        self.stackedobs[...,-observation.shape[-1]:] = observation
+        self.old_obs = observation
+        return self.stackedobs.copy(), reward, done, info
+
+    def reset(self, **kwargs):
+        observation = self.env.reset(**kwargs)
+        self.stackedobs = np.tile(observation, 2)
+        return self.stackedobs.copy()
     
 def maybe_framestack(config):
     config_copy = config.copy()
     fs = config_copy.pop('frame_stack')
-    if fs > 1:
+    if fs == 2:
+        return FasterFrameStack2(RewardMonitor(ProcgenEnvWrapper(config_copy)))
+    elif fs > 1:
         return FrameStackByChannels(RewardMonitor(ProcgenEnvWrapper(config_copy)), fs)
     else:
         return RewardMonitor(ProcgenEnvWrapper(config_copy))
