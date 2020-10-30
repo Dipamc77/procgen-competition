d28d29d6350d4f0f6bd0bddaa9f0c39fc7e4c4d3
r2d2 <dipamc77@gmail.com>
2020-10-30 17:48:02 +0530

    ppo vf coeff experiment

---
+++ algorithms/ppo_experimental/utils.py
@@ -0,0 +1,179 @@
+import numpy as np
+from ray.rllib.utils import try_import_torch
+from collections import deque
+from skimage.util import view_as_windows
+
+torch, nn = try_import_torch()
+
+def neglogp_actions(pi_logits, actions):
+    return nn.functional.cross_entropy(pi_logits, actions, reduction='none')
+
+def sample_actions(logits, device):
+    u = torch.rand(logits.shape, dtype=logits.dtype).to(device)
+    return torch.argmax(logits - torch.log(-torch.log(u)), dim=1)
+
+def pi_entropy(logits):
+    a0 = logits - torch.max(logits, dim=1, keepdim=True)[0]
+    ea0 = torch.exp(a0)
+    z0 = torch.sum(ea0, dim=1, keepdim=True)
+    p0 = ea0 / z0
+    return torch.sum(p0 * (torch.log(z0) - a0), axis=1)
+
+def roll(arr):
+    s = arr.shape
+    return arr.swapaxes(0, 1).reshape(s[0] * s[1], *s[2:])
+
+def unroll(arr, targetshape):
+    s = arr.shape
+    return arr.reshape(*targetshape, *s[1:]).swapaxes(0, 1)
+
+def safe_mean(xs):
+    return -np.inf if len(xs) == 0 else np.mean(xs)
+
+
+def pad_and_random_crop(imgs, out, pad):
+    """
+    Vectorized pad and random crop
+    Assumes square images?
+    args:
+    imgs: shape (B,H,W,C)
+    out: output size (e.g. 64)
+    """
+    # n: batch size.
+    imgs = np.pad(imgs, [[0, 0], [pad, pad], [pad, pad], [0, 0]])
+    n = imgs.shape[0]
+    img_size = imgs.shape[1] # e.g. 64
+    crop_max = img_size - out
+    w1 = np.random.randint(0, crop_max, n)
+    h1 = np.random.randint(0, crop_max, n)
+    # creates all sliding window
+    # combinations of size (out)
+    windows = view_as_windows(imgs, (1, out, out, 1))[..., 0,:,:, 0]
+    # selects a random window
+    # for each batch element
+    cropped = windows[np.arange(n), w1, h1]
+    cropped = cropped.transpose(0,2,3,1)
+    return cropped
+
+def random_cutout_color(imgs, min_cut, max_cut):
+    n, h, w, c = imgs.shape
+    w1 = np.random.randint(min_cut, max_cut, n)
+    h1 = np.random.randint(min_cut, max_cut, n)
+    
+    cutouts = np.empty((n, h, w, c), dtype=imgs.dtype)
+    rand_box = np.random.randint(0, 255, size=(n, c), dtype=imgs.dtype)
+    for i, (img, w11, h11) in enumerate(zip(imgs, w1, h1)):
+        cut_img = img.copy()
+        # add random box
+        cut_img[h11:h11 + h11, w11:w11 + w11, :] = rand_box[i]
+        
+        cutouts[i] = cut_img
+    return cutouts
+
+def linear_schedule(initial_val, final_val, current_steps, total_steps):
+    frac = 1.0 - current_steps / total_steps
+    return (initial_val-final_val) * frac + final_val
+
+def horizon_to_gamma(horizon):
+    return 1.0 - 1.0/horizon
+    
+class AdaptiveDiscountTuner:
+    def __init__(self, gamma, momentum=0.98, eplenmult=1):
+        self.gamma = gamma
+        self.momentum = momentum
+        self.eplenmult = eplenmult
+        
+    def update(self, horizon):
+        if horizon > 0:
+            htarg = horizon * self.eplenmult
+            gtarg = horizon_to_gamma(htarg)
+            self.gamma = self.gamma * self.momentum + gtarg * (1-self.momentum)
+        return self.gamma
+
+class RetuneSelector:
+    def __init__(self, nbatch, ob_space, ac_space, skips = 800_000, replay_size = 200_000, num_retunes = 5):
+        self.skips = skips + (-skips) % nbatch
+        self.replay_size = replay_size + (-replay_size) % nbatch
+        self.batch_size = nbatch
+        self.batches_in_replay = self.replay_size // nbatch
+        
+        self.num_retunes = num_retunes
+        self.ac_space = ac_space
+        self.ob_space = ob_space
+        
+        self.cooldown_counter = self.skips // self.batch_size
+        self.replay_index = 0
+        self.buffer_full = False
+
+    def update(self, obs_batch, exp_replay):
+        if self.num_retunes == 0:
+            return False
+        
+        if self.cooldown_counter > 0:
+            self.cooldown_counter -= 1
+            return False
+        
+        start = self.replay_index * self.batch_size
+        end = start + self.batch_size
+        exp_replay[start:end] = obs_batch
+        
+        self.replay_index = (self.replay_index + 1) % self.batches_in_replay
+        self.buffer_full = self.buffer_full or (self.replay_index == 0)
+        
+        return self.buffer_full
+        
+    def retune_done(self):
+        self.cooldown_counter = self.skips // self.batch_size
+        self.num_retunes -= 1
+        self.replay_index = 0
+        self.buffer_full = False
+        
+    def set_num_retunes(self, nr):
+        self.num_retunes = nr
+
+class RewardNormalizer(object):
+    # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
+    def __init__(self, gamma=0.99, cliprew=10.0, epsilon=1e-8):
+        self.epsilon = epsilon
+        self.gamma = gamma
+        self.ret_rms = RunningMeanStd(shape=())
+        self.cliprew = cliprew
+        self.ret = 0. # size updates after first pass
+        
+    def normalize(self, rews, news, reset_returns=True):
+        self.ret = self.ret * self.gamma + rews
+        self.ret_rms.update(self.ret)
+        rews = np.clip(rews / np.sqrt(self.ret_rms.var + self.epsilon), -self.cliprew, self.cliprew)
+        if reset_returns:
+            self.ret[np.array(news, dtype=bool)] = 0. ## Values should be True of False to set positional index
+        return rews
+    
+class RunningMeanStd(object):
+    # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
+    def __init__(self, epsilon=1e-4, shape=()):
+        self.mean = np.zeros(shape, 'float64')
+        self.var = np.ones(shape, 'float64')
+        self.count = epsilon
+
+    def update(self, x):
+        batch_mean = np.mean(x, axis=0)
+        batch_var = np.var(x, axis=0)
+        batch_count = x.shape[0]
+        self.update_from_moments(batch_mean, batch_var, batch_count)
+
+    def update_from_moments(self, batch_mean, batch_var, batch_count):
+        self.mean, self.var, self.count = update_mean_var_count_from_moments(
+            self.mean, self.var, self.count, batch_mean, batch_var, batch_count)
+
+def update_mean_var_count_from_moments(mean, var, count, batch_mean, batch_var, batch_count):
+    delta = batch_mean - mean
+    tot_count = count + batch_count
+
+    new_mean = mean + delta * batch_count / tot_count
+    m_a = var * count
+    m_b = batch_var * batch_count
+    M2 = m_a + m_b + np.square(delta) * count * batch_count / tot_count
+    new_var = M2 / tot_count
+    new_count = tot_count
+
+    return new_mean, new_var, new_count
---
+++ models/impala_ppo_experimental.py
@@ -0,0 +1,176 @@
+from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+from ray.rllib.models import ModelCatalog
+from ray.rllib.utils.annotations import override
+from ray.rllib.utils import try_import_torch
+import numpy as np
+
+torch, nn = try_import_torch()
+
+class D2RL(nn.Module):
+    def __init__(self, num_inputs, num_outputs, final_layer_gain, hidden_dim):
+        super().__init__()
+        
+        in_dim = num_inputs + hidden_dim
+        self.inp_layer = nn.Linear(num_inputs, hidden_dim)
+        
+        self.layer_1 = nn.Linear(in_dim, hidden_dim)
+        self.layer_2 = nn.Linear(in_dim, hidden_dim)
+        self.layer_3 = nn.Linear(in_dim, hidden_dim)
+            
+        self.out_layer = nn.Linear(hidden_dim, num_outputs)
+        nn.init.orthogonal_(self.out_layer.weight, gain=final_layer_gain)
+        nn.init.zeros_(self.out_layer.bias)
+        
+    def forward(self, latent):
+        x = self.inp_layer(latent)
+        x = nn.functional.relu(x)
+        
+        x = torch.cat([x, latent], dim=1)
+        x = nn.functional.relu(self.layer_1(x))
+        x = torch.cat([x, latent], dim=1)
+        x = nn.functional.relu(self.layer_2(x))
+        x = torch.cat([x, latent], dim=1)
+        x = nn.functional.relu(self.layer_3(x))
+        
+        return self.out_layer(x)
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.conv0 = nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=3, padding=1)
+    
+    def forward(self, x):
+        inputs = x
+        x = nn.functional.relu(x)
+        x = self.conv0(x)
+        x = nn.functional.relu(x)
+        x = self.conv1(x)
+        return x + inputs
+
+
+class ConvSequence(nn.Module):
+    def __init__(self, input_shape, out_channels):
+        super().__init__()
+        self._input_shape = input_shape
+        self._out_channels = out_channels
+        self.conv = nn.Conv2d(in_channels=self._input_shape[0], out_channels=self._out_channels, kernel_size=3, padding=1)
+        self.res_block0 = ResidualBlock(self._out_channels)
+        self.res_block1 = ResidualBlock(self._out_channels)
+
+    def forward(self, x, pool=True):
+        x = self.conv(x)
+        if pool:
+            x = nn.functional.max_pool2d(x, kernel_size=3, stride=2, padding=1)
+        x = self.res_block0(x)
+        x = self.res_block1(x)
+        assert x.shape[1:] == self.get_output_shape()
+        return x
+
+    def get_output_shape(self):
+        _c, h, w = self._input_shape
+        return (self._out_channels, (h + 1) // 2, (w + 1) // 2)
+
+
+class ImpalaCNN(TorchModelV2, nn.Module):
+    """
+    Network from IMPALA paper implemented in ModelV2 API.
+
+    Based on https://github.com/ray-project/ray/blob/master/rllib/models/tf/visionnet_v2.py
+    and https://github.com/openai/baselines/blob/9ee399f5b20cd70ac0a871927a6cf043b478193f/baselines/common/models.py#L28
+    """
+
+    def __init__(self, obs_space, action_space, num_outputs, model_config,
+                 name, device):
+        TorchModelV2.__init__(self, obs_space, action_space, num_outputs,
+                              model_config, name)
+        nn.Module.__init__(self)
+
+        self.device = device
+        depths = model_config['custom_model_config'].get('depths')
+        nlatents = model_config['custom_model_config'].get('nlatents')
+        d2rl = model_config['custom_model_config'].get('d2rl')
+        self.use_layernorm = model_config['custom_model_config'].get('use_layernorm')
+        self.diff_framestack = model_config['custom_model_config'].get('diff_framestack')
+
+        
+        h, w, c = obs_space.shape
+        if self.diff_framestack:
+            assert c == 6, "diff_framestack is only for frame_stack = 2"
+            c = 9
+        shape = (c, h, w)
+
+        conv_seqs = []
+        for out_channels in depths:
+            conv_seq = ConvSequence(shape, out_channels)
+            shape = conv_seq.get_output_shape()
+            conv_seqs.append(conv_seq)
+        self.conv_seqs = nn.ModuleList(conv_seqs)
+        self.hidden_fc = nn.Linear(in_features=shape[0] * shape[1] * shape[2], out_features=nlatents)
+        if not d2rl:
+            self.pi_fc = nn.Linear(in_features=nlatents, out_features=num_outputs)
+            self.value_fc = nn.Linear(in_features=nlatents, out_features=1)
+            nn.init.orthogonal_(self.pi_fc.weight, gain=0.01)
+            nn.init.orthogonal_(self.value_fc.weight, gain=1)
+            nn.init.zeros_(self.pi_fc.bias)
+            nn.init.zeros_(self.value_fc.bias)
+        else:
+            self.pi_fc = D2RL(num_inputs=nlatents, num_outputs=num_outputs, 
+                              final_layer_gain=0.01, hidden_dim=256)
+            self.value_fc = D2RL(num_inputs=nlatents, num_outputs=1, 
+                                 final_layer_gain=1, hidden_dim=256)
+
+        if self.use_layernorm:
+            self.layernorm = nn.LayerNorm(nlatents)
+
+    
+    @override(TorchModelV2)
+    def forward(self, input_dict, state, seq_lens):
+        x = input_dict["obs"].float()
+        if self.diff_framestack:
+            x = torch.cat([x,  x[...,:-3] - x[...,-3:]], dim=3) # only works for framestack 2 for now
+        x = x / 255.0  # scale to 0-1
+        x = x.permute(0, 3, 1, 2)  # NHWC => NCHW
+        x = self.conv_seqs[0](x)
+        x = self.conv_seqs[1](x)
+        x = self.conv_seqs[2](x)
+        x = torch.flatten(x, start_dim=1)
+        x = nn.functional.relu(x)
+        x = self.hidden_fc(x)
+        if self.use_layernorm:
+            x = self.layernorm(x)
+            x = torch.tanh(x)
+        else:
+            x = nn.functional.relu(x)
+        logits = self.pi_fc(x)
+        value = self.value_fc(x)
+        self._value = value.squeeze(1)
+        self._detached_latent = x.detach()
+        return logits, state
+
+    @override(TorchModelV2)
+    def value_function(self):
+        assert self._value is not None, "must call forward() first"
+        return self._value
+    
+    def vf_pi(self, obs, no_grad=False, ret_numpy=False, to_torch=False):
+        if to_torch:
+            obs = torch.tensor(obs).to(self.device)
+            
+        def v_pi(obs):
+            pi, _ = self.forward({"obs": obs}, None, None)
+            v = self.value_function()
+            return v, pi
+        
+        if no_grad:
+            with torch.no_grad():
+                v, pi = v_pi(obs)
+        else:
+            v, pi = v_pi(obs)
+        
+        if ret_numpy:
+            return v.cpu().numpy(), pi.cpu().numpy()
+        else:
+            return v, pi
+    
+ModelCatalog.register_custom_model("impala_ppo_experimental", ImpalaCNN)
